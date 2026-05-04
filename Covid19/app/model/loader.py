731dff968ff37d3fb8d597535752859905c911f0
Farida Helmy <faridahelmy28@gmail.com>
2026-05-04 16:05:31 +0300

    Refactor model loading logic in loader.py
    Refactor model loading to include directory creation and use a specific file ID for downloading.

--- Covid19/app/model/loader.py
+++ Covid19/app/model/loader.py
@@ -4,6 +4,11 @@ from tensorflow.keras.models import load_model
 
 MODEL_PATH = "app/model/model.h5"
 
-if not os.path.exists(MODEL_PATH):
-    url = "https://drive.google.com/drive/home?dmr=1&ec=wgc-drive-%5Bmodule%5D-goto"
-    gdown.download(url, MODEL_PATH, quiet=False)
+def load_trained_model():
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+        file_id = "17dxS2gRw2Gw1mt6-1bindlL7GsvQBuvd"
+        url = f"https://drive.google.com/uc?id={file_id}&export=download"
+        gdown.download(url, MODEL_PATH, quiet=False)
+    
+    return load_model(MODEL_PATH)

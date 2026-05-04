8bef8bedf40cd5fd42d2fd3c549e04461cf481d6
Farida Helmy <faridahelmy28@gmail.com>
2026-05-04 15:59:51 +0300

    Delete Covid19/loader.py

--- Covid19/loader.py
+++
@@ -1,9 +0,0 @@
-import os
-import gdown
-from tensorflow.keras.models import load_model
-
-MODEL_PATH = "app/model/model.h5"
-
-if not os.path.exists(MODEL_PATH):
-    url = "https://drive.google.com/file/d/17dxS2gRw2Gw1mt6-1bindlL7GsvQBuvd/view?usp=sharing"
-    gdown.download(url, MODEL_PATH, quiet=False)
